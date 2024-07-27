293773185b8a130f12e5cbd0a8d5bbbd86934e0d
ecc97 <carmonaedwin1123@gmail.com>
2024-07-27 09:57:32 -0500

    feat: pagination function

--- dist/components/index.js
+++ dist/components/index.js
@@ -1,6 +1,10 @@
 "use strict";
 const inputFile = document.getElementById('csv-file');
 const tableContainer = document.querySelector('#table-container');
+const paginationContainer = document.querySelector('#pagination');
+const recordsPerPage = 15;
+let currentPage = 1;
+let csvData = '';
 function handleFileSelect(event) {
     var _a;
     const input = event.target;
@@ -27,14 +31,18 @@ function handleFileSelect(event) {
         const table = document.createElement('table');
         table.id = 'csv-table';
         table.classList.add('table', 'table-striped');
-        tableContainer === null || tableContainer === void 0 ? void 0 : tableContainer.appendChild(table);
+        tableContainer.appendChild(table);
         createTable(csvText);
+        pagination(csvText);
     };
     reader.readAsText(file);
 }
 function createTable(data) {
     const table = document.getElementById('csv-table');
+    table.innerHTML = '';
     const rows = data.split('\n').map(row => row.split(','));
+    const start = (currentPage - 1) * recordsPerPage + 1;
+    const end = Math.min(start + recordsPerPage - 1, rows.length - 1);
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
     const headerRow = rows[0];
@@ -45,8 +53,22 @@ function createTable(data) {
         trHead.appendChild(th);
     });
     thead.appendChild(trHead);
-    rows.slice(1).forEach(row => {
+    // usando for 
+    // for (let i = start; i <= end; i++){
+    //     const row = rows[i];
+    //     const tr = document.createElement('tr') as HTMLTableRowElement;
+    //     row.forEach(cell => {
+    //         const td = document.createElement('td') as HTMLTableCellElement;
+    //         td.textContent = cell;
+    //         tr.appendChild(td);
+    //     });
+    //     tbody.appendChild(tr);
+    // }
+    //usando foreach
+    const arrayAsc = [];
+    rows.slice(start, end + 1).forEach(row => {
         const tr = document.createElement('tr');
+        arrayAsc.push(row[4]);
         row.forEach(cell => {
             const td = document.createElement('td');
             td.textContent = cell;
@@ -54,7 +76,27 @@ function createTable(data) {
         });
         tbody.appendChild(tr);
     });
+    console.log(arrayAsc.reverse());
     table.appendChild(thead);
     table.appendChild(tbody);
+}
+function pagination(data) {
+    const previousButton = document.querySelector("#previous");
+    const nextButton = document.querySelector("#next");
+    previousButton.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--;
+            console.log(currentPage);
+            createTable(data);
+        }
+    });
+    nextButton.addEventListener('click', () => {
+        const maxPage = Math.ceil(data.split('\n').length / recordsPerPage);
+        if (currentPage < maxPage) {
+            currentPage++;
+            console.log(currentPage);
+            createTable(data);
+        }
+    });
 }
 inputFile.addEventListener('change', handleFileSelect);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,14 @@
 const inputFile = document.getElementById('csv-file') as HTMLInputElement;
 const tableContainer = document.querySelector('#table-container') as HTMLDivElement;
+const paginationContainer = document.querySelector('#pagination') as HTMLLIElement
+
+const recordsPerPage = 15;
+let currentPage = 1;
+let csvData:string = '';
 
 function handleFileSelect(event: Event) {
     const input = event.target as HTMLInputElement;
-    const errorMessage = document.getElementById('errorMessage');
+    const errorMessage = document.getElementById('errorMessage') as HTMLParagraphElement;
 
     if (!input.files?.length) {
         return;
@@ -27,49 +32,94 @@ function handleFileSelect(event: Event) {
         // Mostrar el contenido del CSV tal cual
         console.log(csvText);
 
-        const table = document.createElement('table');
+        const table = document.createElement('table') as HTMLTableElement;
         table.id = 'csv-table';
         table.classList.add('table', 'table-striped')
-        tableContainer?.appendChild(table);
+        tableContainer.appendChild(table);
 
         createTable(csvText);
+        pagination(csvText)
     };
 
     reader.readAsText(file);
 }
 
-function createTable(data: string) {
+function createTable(data: string ) {
     const table = document.getElementById('csv-table') as HTMLTableElement;
+    table.innerHTML = ''
 
-    const rows = data.split('\n').map(row => row.split(','));
+    const rows: string[][] = data.split('\n').map(row => row.split(','));
 
-    const thead = document.createElement('thead');
-    const tbody = document.createElement('tbody');
+    const start = (currentPage - 1) * recordsPerPage + 1
+    const end = Math.min(start + recordsPerPage - 1, rows.length -1)
+
+    const thead = document.createElement('thead') as HTMLTableSectionElement;
+    const tbody = document.createElement('tbody') as HTMLTableSectionElement;
     
-    const headerRow = rows[0];
-    const trHead = document.createElement('tr');
+    const headerRow: string[] = rows[0];
+    const trHead = document.createElement('tr') as HTMLTableRowElement;
     headerRow.forEach(cell => {
-        const th = document.createElement('th');
+        const th = document.createElement('th') as HTMLTableCellElement;
         th.textContent = cell;
         trHead.appendChild(th);
     });
     thead.appendChild(trHead);
 
-    
-    rows.slice(1).forEach(row => {
-        const tr = document.createElement('tr');
+    // usando for 
+    // for (let i = start; i <= end; i++){
+    //     const row = rows[i];
+    //     const tr = document.createElement('tr') as HTMLTableRowElement;
+    //     row.forEach(cell => {
+    //         const td = document.createElement('td') as HTMLTableCellElement;
+    //         td.textContent = cell;
+    //         tr.appendChild(td);
+    //     });
+    //     tbody.appendChild(tr);
+    // }
+
+    //usando foreach
+    const arrayAsc: string[] = []
+    rows.slice(start, end + 1).forEach(row => {
+        const tr = document.createElement('tr') as HTMLTableRowElement;
+        arrayAsc.push(row[4])
+        
         row.forEach(cell => {
-            const td = document.createElement('td');
+            const td = document.createElement('td') as HTMLTableCellElement;
             td.textContent = cell;
             tr.appendChild(td);
         });
         tbody.appendChild(tr);
     });
-
     
+    console.log(arrayAsc.reverse())
     table.appendChild(thead);
     table.appendChild(tbody);
+
 }
+
+function pagination(data: string) {
+    const previousButton = document.querySelector("#previous") as HTMLButtonElement;
+    const nextButton = document.querySelector("#next") as HTMLButtonElement
+
+    previousButton.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--
+            console.log(currentPage)
+            createTable(data)
+        }
+    })
+
+    nextButton.addEventListener('click', () => {
+        const maxPage = Math.ceil(data.split('\n').length / recordsPerPage);
+        if (currentPage < maxPage) {
+            currentPage++;
+            console.log(currentPage)
+            createTable(data)
+        }
+    })
+}
+
+
 
 inputFile.addEventListener('change', handleFileSelect);
 
