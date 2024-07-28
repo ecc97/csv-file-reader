bca65ec6f95ceb56ce0ce1723de0df70c3e70272
ecc97 <carmonaedwin1123@gmail.com>
2024-07-27 21:00:27 -0500

    feat: function searchDataCSV added

--- dist/components/index.js
+++ dist/components/index.js
@@ -1,6 +1,7 @@
 import { CSVController } from "../controllers/csv.controller.js";
 const inputFile = document.getElementById('csv-file');
+const searchInput = document.getElementById('searchInput');
 const tableContainer = document.querySelector('#table-container');
 const paginationContainer = document.querySelector('#pagination');
-const csvController = new CSVController(inputFile, tableContainer, paginationContainer);
+const csvController = new CSVController(inputFile, searchInput, tableContainer, paginationContainer);
 csvController.initPagination();
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import { CSVController } from "../controllers/csv.controller.js";
 
 const inputFile = document.getElementById('csv-file') as HTMLInputElement;
+const searchInput = document.getElementById('searchInput') as HTMLInputElement;
 const tableContainer = document.querySelector('#table-container') as HTMLDivElement;
 const paginationContainer = document.querySelector('#pagination') as HTMLDivElement;
 
-const csvController = new CSVController(inputFile, tableContainer, paginationContainer);
+const csvController = new CSVController(inputFile, searchInput, tableContainer, paginationContainer);
 
 csvController.initPagination();
